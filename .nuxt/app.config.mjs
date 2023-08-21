5a74b5b42d920ef8b061719f2feb95f11e6ff491
haalahibrahim03 <mdl19ebe025@mec.ac.in>
2023-08-21 11:40:00 +0530

    new changes

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/haalahibrahim/Desktop/NuxtEampleSite/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default defuFn(inlineConfig)
---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///Users/haalahibrahim/Desktop/NuxtEampleSite/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
